e8a726cf8a27774a3ef061e19341495e7ff22a9b
louisgurung <louisgurung@yahoo.com>
2020-01-08 19:58:17 +0545

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
-import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./Components/movies";
+import Customers from "./Components/customers";
+import Rentals from "./Components/rentals";
+import NotFound from "./Components/notFound";
+import NavBar from "./Components/navBar";
+import "./App.css";
+import MovieForm from "./Components/movieForm";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar></NavBar>
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
